c1dd874b90d3f7f7464e145c35c97274dbad8fa4
Makoto Hirose <makolllmako@gmail.com>
2019-08-07 17:58:04 +0000

    スクロール追加

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import WelcomeScreen from './screens/WelcomeScreen';
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles2.container}>React Native Test App!</Text>
+      <WelcomeScreen />
     </View>
   );
 }
