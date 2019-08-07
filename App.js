1613f54ed475970de374888e5644d8793ac636d2
Makoto Hirose <makolllmako@gmail.com>
2019-08-07 18:42:18 +0000

    feat: 緯度経度を表示

--- App.js
+++ App.js
@@ -1,15 +1,42 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
 
 import WelcomeScreen from './screens/WelcomeScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles2.container}>React Native Test App!</Text>
-      <WelcomeScreen />
-    </View>
-  );
+export default class App extends Component {
+  state = {
+    locationResult: null,
+  };
+
+  componentDidMount() {
+    this._getLocationAsync();
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        locationResult: 'Permission to access location was denied',
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    console.log(JSON.stringify(location));
+    this.setState({ locationResult: JSON.stringify(location) });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles2.container}>React Native Test App!</Text>
+        <Text>{this.state.locationResult}</Text>
+        <WelcomeScreen />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
